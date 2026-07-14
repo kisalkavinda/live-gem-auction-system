9317d196f3e46cc21052ad3706dae38c011166b5
Avishka <eaahashara01@gmail.com>
2026-07-15 00:18:56 +0530

    Fix product detailed view removing mock connections

--- frontend/src/pages/GemDetailPage.jsx
+++ frontend/src/pages/GemDetailPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { gsap } from '../utils/gsap'
-import { fetchGemById } from '../data/mockGems'
+import { fetchGemById } from '../services/gemService'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
